006dd28d294cb28a768614297bb6771e31dafb8d
Tiago Candido <tiagocan@gmail.com>
2025-10-02 22:43:22 -0300

    feat: criado ação botao iniciar quiz + PerguntasActivity

--- app/src/main/java/com/mentoriaandroid/appquiz/MainActivity.kt
+++ app/src/main/java/com/mentoriaandroid/appquiz/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mentoriaandroid.appquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editNome: EditText
+    private lateinit var btnIniciar: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editNome = findViewById(R.id.edit_nome)
+        btnIniciar = findViewById(R.id.btn_iniciar)
+
+        btnIniciar.setOnClickListener {
+
+            val nome = editNome.text.toString()
+
+            if ( nome.isNotEmpty() ){//"jamilton" -> true
+                val intent = Intent(this, PerguntasActivity::class.java)
+                intent.putExtra("nome_usuario", nome)
+
+                startActivity( intent )
+            }else{
+                Toast.makeText(this, "Favor preencher o Nome!", Toast.LENGTH_SHORT).show()
+                
+            }
+
+        }
+
     }
 }
